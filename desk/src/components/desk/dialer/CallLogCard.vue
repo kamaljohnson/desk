0fe25cf144cd69ec677901e33c1e79f927261d48
kamaljohnson <kamaljohnson12345@gmail.com>
2022-12-08 01:27:53 +0530

    refactor: call log card ui

--- desk/src/components/desk/dialer/CallLogCard.vue
+++ desk/src/components/desk/dialer/CallLogCard.vue
@@ -1,13 +1,24 @@
 <template>
 	<div
-		v-if="callLog"
-		class="flex flex-col my-[16px] bg-gray-100 rounded-[6px] p-[10px] text-base"
+		v-if="callLog && agent"
+		class="flex flex-col my-[16px] px-[8px] text-base"
 	>
 		<div class="flex flex-row justify-between">
-			<div>
-				<div>{{ `From: ${callLog.agent_ref}` }}</div>
-				<div>{{ `To: ${callLog.to}` }}</div>
-				<div>{{ `Status: ${callLog.status}` }}</div>
+			<div class="grow flex flex-col space-y-2">
+				<div class="flex flex-row items-center space-x-2">
+					<div class="p-1.5 rounded-[6px] h-fit">
+						<FeatherIcon
+							name="phone-outgoing"
+							class="h-3 w-3 stroke-red-500"
+						/>
+					</div>
+					<div class="font-medium text-base text-gray-900">
+						{{ agent.agent_name }}
+					</div>
+				</div>
+				<div class="pl-[32px]">
+					<div class="text-gray-500">{{ callLog.status }}</div>
+				</div>
 			</div>
 			<div class="text-gray-500 font-normal text-[12px]">
 				{{ $dayjs.longFormating($dayjs(callLog.creation).fromNow()) }}
@@ -18,12 +29,28 @@
 
 <script>
 import { FeatherIcon } from "frappe-ui"
+import { inject, computed } from "vue"
 
 export default {
 	name: "CallLogCard",
 	props: ["callLog"],
 	components: {
 		FeatherIcon,
+	},
+	setup(props) {
+		const $socket = inject("$socket")
+		const $agents = inject("$agents")
+		const agent = computed(() => {
+			if (!props.callLog) return
+			return $agents.get(
+				{ agentId: props.callLog.reference_agent },
+				{ $socket }
+			).value
+		})
+
+		return {
+			agent,
+		}
 	},
 }
 </script>
