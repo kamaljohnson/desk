bd54fe0bb2394a5ccd584d38a524e7d822cd4a9f
kamaljohnson <kamaljohnson12345@gmail.com>
2022-12-13 00:53:08 +0530

    feat: add call recording audio clip ui

--- desk/src/components/desk/dialer/CallLogCard.vue
+++ desk/src/components/desk/dialer/CallLogCard.vue
@@ -1,7 +1,7 @@
 <template>
 	<div
 		v-if="callLog && agent"
-		class="flex flex-col my-[16px] px-[8px] text-base"
+		class="flex flex-col my-[16px] px-[8px] text-base bg-red-50 py-2 rounded-[6px]"
 	>
 		<div class="flex flex-row justify-between">
 			<div class="grow flex flex-col space-y-2">
@@ -12,17 +12,32 @@
 							class="h-3 w-3 stroke-red-500"
 						/>
 					</div>
-					<div class="font-medium text-base text-gray-900">
-						{{ agent.agent_name }}
+					<div class="flex flex-row space-x-3 items-center">
+						<div
+							class="font-medium text-base text-gray-900 flex flex-row space-x-2 items-center"
+						>
+							<div>
+								{{ agent.agent_name }}
+							</div>
+							<FeatherIcon name="arrow-right" class="h-3" />
+							<div>
+								{{ contact.first_name }}
+							</div>
+						</div>
+						<div class="text-gray-500 font-normal">
+							({{ callLog.status }})
+						</div>
 					</div>
-				</div>
-				<div class="pl-[32px]">
-					<div class="text-gray-500">{{ callLog.status }}</div>
 				</div>
 			</div>
 			<div class="text-gray-500 font-normal text-[12px]">
 				{{ $dayjs.longFormating($dayjs(callLog.creation).fromNow()) }}
 			</div>
+		</div>
+		<div class="flex flex-row space-x-1 items-center ml-[20px]">
+			<audio v-if="callLog.recording_url" controls>
+				<source :src="callLog.recording_url" type="audio/ogg" />
+			</audio>
 		</div>
 	</div>
 </template>
@@ -40,6 +55,9 @@ export default {
 		agent() {
 			return this.$resources.agent.doc || null
 		},
+		contact() {
+			return this.$resources.contact.doc || null
+		},
 	},
 	resources: {
 		agent() {
@@ -49,6 +67,40 @@ export default {
 				name: this.callLog.reference_agent,
 			}
 		},
+		contact() {
+			return {
+				type: "document",
+				doctype: "Contact",
+				name: this.callLog.reference_contact,
+			}
+		},
 	},
 }
 </script>
+
+<style>
+audio {
+	/*border-radius: 90px;*/
+	width: 100%;
+	height: 35px;
+}
+
+audio::-webkit-media-controls-mute-button {
+	display: none !important;
+}
+
+audio::-webkit-media-controls-volume-slider {
+	display: none !important;
+}
+
+audio::-webkit-media-controls-volume-control-container.closed {
+	display: none !important;
+}
+audio::-webkit-media-controls-volume-control-container {
+	display: none !important;
+}
+audio::-webkit-media-controls-panel {
+	background: rgb(254, 242, 242);
+	padding: 0px;
+}
+</style>
